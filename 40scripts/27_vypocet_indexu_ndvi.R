ba29fa84837fe74ddad97d1a47f2862827447fe4
ledvinkao <stathydro.cz@gmail.com>
2024-10-13 22:27:32 +0200

    ndvi script added

---
+++ 40scripts/27_vypocet_indexu_ndvi.R
@@ -0,0 +1,28 @@
+
+# Výpočet indexu NDVI -----------------------------------------------------
+
+# asi nečastější se satelitními snímky jsou výpočty spektrálích indexů
+# jedním z nejužívanějších (a snad i nadužívaných) indexů je normalizovaný diferencovaný vegetační index (NDVI)
+# ten vychází ze dvou pásem - blízkého infračerveného (u Landsatu 7 pásmo 4) a červeného (u Landsatu 7 pásmo 3)
+
+# načteme potřebné balíčky
+xfun::pkg_attach("tidyverse",
+                 "terra",
+                 "tidyterra",
+                 install = T)
+
+# načteme Landsat data
+landsat <- rast(system.file("tif/L7_ETMs.tif",
+                            package = "stars"))
+
+# za využití jednoduchého výrazu pro NDVI (viz https://en.wikipedia.org/wiki/Normalized_difference_vegetation_index), lze pokračovat následovně
+ndvi <- (landsat[[4]] - landsat[[3]]) / (landsat[[4]] + landsat[[3]])
+
+# výslednou vrstvu si ještě můžeme přejmenovat
+names(ndvi) <- "ndvi"
+
+# vrstvu si můžeme vykreslit funkcemi podporovanými v pojetí ggplot
+ggplot() + 
+  geom_spatraster(data = ndvi) +
+  scale_fill_distiller(palette = "RdYlBu",
+                       direction = -1)
