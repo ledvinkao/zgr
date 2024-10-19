2f15e9d1e4e289892f71156a57991dbbc0b3510a
ledvinkao <stathydro.cz@gmail.com>
2024-10-19 14:06:12 +0200

    script 39 corrected (new link, filtering to non-zero areas, etc.)

--- 40scripts/39_zisk_polygonu_povodi_nad_vodomernymi_stanicemi.R
+++ 40scripts/39_zisk_polygonu_povodi_nad_vodomernymi_stanicemi.R
@@ -11,30 +11,31 @@ xfun::pkg_attach("tidyverse",
 # předpokládá se, že známe adresu polygonové vrstvy s rozvodnicemi
 # adresu včetně čísla vrstvy najdeme mezi otevřenými prostorovými daty ĆHMÚ (https://open-data-chmi.hub.arcgis.com/)
 # klidně si dovolíme načíst celou nejpodrobnější vrstvu s rozšířenými rozvodnicemi 4. řádu
+# někdy je potřeba následující řádky spustit vícekrát, aby proces načítání skončil úspěchem
 # nakonec ještě převádíme na přehlednější tibble simple feature
 # jen pro ukázku si děláme pořádek v chybějících hodnotách v textových polích
-rozv <- arc_read("https://services1.arcgis.com/ZszVN9lBVA5x4VmX/arcgis/rest/services/rozvodnice_IV_radu_full/FeatureServer/0") |> 
+rozv <- arc_read("https://services1.arcgis.com/ZszVN9lBVA5x4VmX/arcgis/rest/services/rozvodnice5G_4_radu_plus/FeatureServer/6") |> 
   as_tibble() |> 
   st_sf() |> 
   mutate(across(where(is.character),
                 \(x) if_else(x == "", NA, x)))
 
 # omezíme se jen na řádky, kde je stanice
-# jelikož ČHMÚ má fungující stanice označeny pomocí identifikátory připomínající šestimístné číslo (s vodícími nulami), dovolíme si využít vedlejšího efektu
+# jelikož ČHMÚ pro fungující stanice používá identifikátory připomínající šestimístné číslo (s vodícími nulami), dovolíme si využít vedlejšího efektu
 stanice <- rozv |> 
-  filter(!is.na(as.numeric(DBCN))) |> 
+  filter(!is.na(as.numeric(dbcn))) |> 
   st_drop_geometry() # protože geometrii nepotřebujeme a jen by zdržovala
 
 # tohoto pomocneho objektu využijeme k vyběru všech menších polygonů nad stanicemi
 # výsledky si vytvoříme jako nový sloupec tabulky, abychom měli všechno pěkně přiřazené
 stanice <- stanice |> 
-  group_by(id = DBCN) |> 
+  group_by(id = dbcn) |> 
   nest()
 
 # využíváme znalosti atributové tabulky a podmínku příslušnosti povodí nad vodoměrnou stanicí
 stanice <- stanice |> 
   mutate(data2 = data |>
-           map(\(x) filter(rozv, CHP_14_S >= x$CHP_14_S_U & CHP_14_S <= x$CHP_14_S))
+           map(\(x) filter(rozv, chp_14_s >= x$chp_14_s_u & chp_14_s <= x$chp_14_s))
   )
 
 # pro jednotlivé stanice provedeme sjednocení geometrie
@@ -45,20 +46,13 @@ plan(multisession,
      workers = availableCores() - 1) # raději si ještě jeden zbývající procesor necháváme stranou (příliš pracovníků se může prát o RAM a zpomalovat tím výpočet)
 
 stanice <- stanice |> 
+  ungroup() |> # někdy je pro paralelní zpracování nutné tabulku zbavit grupování, jinak se spouští proces jen sekvenčně
   mutate(data2 = data2 |> 
            future_map(\(x) st_union(x) |> 
                         st_sf() |> 
                         st_set_geometry("geoms"),
                       .options = furrr_options(seed = NULL, # protože nepotřebujeme generovat pseudonáhodná čísla
-                                               packages = "sf"))) # je potřeba upřesnit, jaké další balíčky poskytují funkce, které ostatní pracovníci neznají
-
-# pro případy, kde je na stroji málo RAM se skutečně vyplatí pracovat klasicky sekvenčně
-stanice <- stanice |>
-  mutate(data2 = data2 |>
-           map(\(x) st_union(x) |>
-                 st_sf() |>
-                 st_set_geometry("geoms"))
-  )
+                                               packages = "sf"))) # je potřeba upřesnit, jaké další balíčky poskytují funkce, které ostatní pracovníci (workers) neznají
 
 # přecházíme zpět na sekvenční zpracování
 plan(sequential)
@@ -73,13 +67,7 @@ stanice <- stanice |>
 stanice |> 
   filter(!st_is_valid(geoms))
 
-# všechny geometrie jsou validní, avšak stále mohou obsahovat díry
-# tak se děr zbavíme
-stanice <- stanice |> 
-  sfheaders::sf_remove_holes()
-
-stanice |> 
-  filter(!st_is_valid(geoms))
+# všechny geometrie jsou validní
 
 # jelikož některá povodí mohou být subpovodími vetších povodí, je vhodné si vrstvu uspořádat tak, aby nejmenší povodí byla v tabulce dole
 # docílíme tím i toho, že se vrstva bude lépe kreslit (s největšími povodími na prvním míste a s nejmenšími dole)
@@ -90,6 +78,11 @@ stanice <- stanice |>
            round(2)) |> 
   arrange(desc(a)) |> 
   relocate(a, .after = id)
+
+# posledním krokem bude odstranění povodí s nulovou plochou
+# takové případy mohou odkazovat na tzv. fiktivní stanice, měření náhonů apod.
+stanice <- stanice |> 
+  filter(a > units::set_units(0, km2)) # musíme porovnávat se správně nastavenými jednotkami
 
 # přitom není potřeba ukládat vrstvu do souboru, který je znám v GIS
 # vhodné je i uložení do RDS souboru (geometrie a vše ostatní po načtení zůstane výhodně zachováno)
