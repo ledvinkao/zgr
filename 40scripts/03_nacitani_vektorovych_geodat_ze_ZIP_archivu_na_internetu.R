618bc00227f7b9ab2ee6422afa460d3bfaccb47a
ledvinkao <stathydro.cz@gmail.com>
2025-03-09 10:51:33 +0100

    skript adapted to a different zip file, since the TGM WRI seemingly changes the links, which does not support reproducibility

--- 40scripts/03_nacitani_vektorovych_geodat_ze_ZIP_archivu_na_internetu.R
+++ 40scripts/03_nacitani_vektorovych_geodat_ze_ZIP_archivu_na_internetu.R
@@ -7,11 +7,18 @@
 xfun::pkg_attach2("tidyverse",
                   "sf")
 
-# k tomu, abychom byli v takovém načítání úspěšní, potřebujeme znát internetovou adresu souboru (končící na ".zip")
+# k tomu, abychom byli v samotném načítání úspěšní, potřebujeme znát internetovou adresu souboru (končící na ".zip")
 # tuto adresu vložíme do naší "načítací" funkce, kde ale navíc použijeme tzv. řetězení prefixů
-nadrze <- read_sf("/vsizip//vsicurl/https://www.dibavod.cz/data/download/dib_A05_Vodni_nadrze.zip",
-                  option = "ENCODING=windows-1250")
+
+# vhodné je před načítáním geodat podívat se, co je v archivu nebo ve složce k dispozici
+# funkcí st_layers() s balíčku sf lze takto složky prohlížet
+# demonstrujme na souboru pro Slovesnko, který na stránkách nabízí Evropská agentura pro životní prostředí (EEA)
+st_layers("/vsizip//vsicurl/https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/slovakia-shapefile/at_download/file/slovakia_shapefile.zip")
+
+# a načteme jednu z vrstev
+sk <- read_sf("/vsizip//vsicurl/https://www.eea.europa.eu/data-and-maps/data/eea-reference-grids-2/gis-files/slovakia-shapefile/at_download/file/slovakia_shapefile.zip",
+                  layer = "sk_10km")
 
 # podrobnosti k těmto postupům lze nalézt na stránkách knihovny GDAL, která je při tomto načítání vektorových geodat využívána
 # viz https://gdal.org/en/latest/user/virtual_file_systems.html
-# povšimněme si např. dvojitých lomítek za částí /vsizip
+# povšimněme si např. dvojitých lomítek za částí /vsizip (není to nutné, ale je to doporučováno)
