a260aedb04879d8e9f1145cfba4aafcaaeb86679
ledvinkao <stathydro.cz@gmail.com>
2025-03-09 16:40:18 +0100

    new script 34 added

---
+++ 40scripts/34_rastry_a_anonymni_funkce_vs_funkce_psane_v_C.R
@@ -0,0 +1,42 @@
+
+# Rastry a anonymní R funkce vs. funkce psané v C++ -----------------------
+
+# když aplikujeme funkci na rastr, je třeba rozlišovat mezi funkcí psanou v C++ a R funkcí
+# anonymní funkce předpokládájí aplikace R funkce a také uvažují jinou strategii s rastry (mají např. tendenci vše zpracovávat v RAM)
+# R funkce jsou zpravidla pomalejší, jak můžeme demonstrovat měřením času
+
+# načteme potřebné balíčky
+xfun::pkg_attach2("tidyverse",
+                  "RCzechia", # balíček sf je načten automaticky
+                  "terra",
+                  "geodata")
+
+# porovnáme různé strategie na výpočtu střední nadmořské výšky v krajích
+dem <- elevation_30s(country = "CZE",
+                     path = "geodata",
+                     mask = F)
+
+kraje <- kraje() |> 
+  as_tibble() |> 
+  st_sf()
+
+# aplikujme anonymní funkci
+tictoc::tic(); kraje_a <- extract(dem,
+                                kraje,
+                                fun = \(x) mean(x) |> 
+                                  round(1),
+                                bind = T) |> 
+  st_as_sf() |> 
+  as_tibble() |> 
+  st_sf(); tictoc::toc()
+
+tictoc::tic(); kraje_b <- extract(dem,
+                                kraje,
+                                fun = mean,
+                                bind = T) |> 
+  st_as_sf() |> 
+  as_tibble() |> 
+  st_sf() |> 
+  mutate(CZE_elv = round(CZE_elv, 1)); tictoc::toc()
+
+# rozdíly se zamozřejmě zvětšují se zvetšujícím se množstvím dat a někdy už vůbec nelze aplikovat postup s anonymní funkcí kvůli limtům RAM
