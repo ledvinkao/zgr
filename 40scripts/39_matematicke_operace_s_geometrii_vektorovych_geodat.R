f26497147b48f07472f42338e6ba4a5a5bf5e56a
ledvinkao <stathydro.cz@gmail.com>
2025-03-10 15:16:38 +0100

    one of te scripts renamed, a new script was establshed, and air quality metadata were added

---
+++ 40scripts/39_matematicke_operace_s_geometrii_vektorovych_geodat.R
@@ -0,0 +1,6 @@
+
+# Matematické operace s geometrií vektorových geodat ----------------------
+
+# geometrický sloupec vektorových geodat je ve své podstatě třídy seznam
+# lze tedy s ním provádět i matematické operace, jako je přičítání nebo násobení
+# tato vlastnost se může hodit např. při systematickém posouvání bodů o několik metrů jakýmkoliv směrem
