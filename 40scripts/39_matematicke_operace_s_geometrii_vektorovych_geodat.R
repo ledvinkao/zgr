9ff5e72e0c80e4299a6a9fbf5187f4b420e8c73b
ledvinkao <stathydro.cz@gmail.com>
2025-03-10 16:06:57 +0100

    script 39 maturing

--- 40scripts/39_matematicke_operace_s_geometrii_vektorovych_geodat.R
+++ 40scripts/39_matematicke_operace_s_geometrii_vektorovych_geodat.R
@@ -2,5 +2,27 @@
 # Matematické operace s geometrií vektorových geodat ----------------------
 
 # geometrický sloupec vektorových geodat je ve své podstatě třídy seznam
-# lze tedy s ním provádět i matematické operace, jako je přičítání nebo násobení
+# lze s ním tedy provádět i matematické operace, jako je přičítání nebo násobení
 # tato vlastnost se může hodit např. při systematickém posouvání bodů o několik metrů jakýmkoliv směrem
+
+# demonstrujme s využitím metadat Úseku kvality ovzduší ČHMÚ
+
+# načteme potřebné balíčky
+xfun::pkg_attach2("tidyverse",
+                  "arrow",
+                  "sf",
+                  "tmap")
+
+# s Apache Parquet soubory lze zacházet podobně jako s databázemi
+aqmeta <- open_dataset("metadata/airquality_metadata_pq")
+
+# řekněme, že se budeme chtít omezit jen na veličinu O3
+o3 <- aqmeta |> 
+  filter(VELICINA_ZKRATKA == "O3") |> 
+  collect()
+
+aqmeta |> 
+  filter(VELICINA_ZKRATKA == "O3") |> 
+  select(AKTIVNI_OD, AKTIVNI_DO) |> 
+  head(5) |> 
+  collect()
