8a023bbd3c7a81cfb4e2db2c8a28726981152ed8
Ondrej Ledvinka <stathydro.cz@gmail.com>
2025-07-22 09:56:00 +0200

    script 02 updated mainly due to showing how to work with the 'layer' argument in functions st_read() and read_sf()

--- 40scripts/02_nacitani_vektorovych_geodat_ze_ZIP_archivu.R
+++ 40scripts/02_nacitani_vektorovych_geodat_ze_ZIP_archivu.R
@@ -1,5 +1,5 @@
 
-# # Načtení vektorových geodat přímo ze ZIP archivu (uloženého) -----------
+# Načtení vektorových geodat přímo ze ZIP archivu (uloženého) -------------
 
 # shapefile zabalený v ZIP archivu je dosti častým jevem
 # pokud máme stažený ZIP archiv na lokálním disku, není třeba jej rozbalovat, ale lze si načíst soubor s vektorovými geodaty z archivu přímo
@@ -19,7 +19,11 @@ nadrze <- st_read("/vsizip/geodata/dib_A05_Vodni_nadrze.zip",
                   options = "ENCODING=windows-1250")
 
 # objevuje se varování, že v ZIP souboru je více souborů k načtení a že byl vybrán ten první
-# to nám ale v tuto chvíli vůbec nevadí (jinak se varování lze zbavit specifikací jména vrstvy v argumentu layer)
+# to nám ale v tuto chvíli vůbec nevadí (jinak se varování lze zbavit specifikací jména vrstvy v argumentu "layer")
+nadrze <- st_read("/vsizip/geodata/dib_A05_Vodni_nadrze.zip",
+                  options = "ENCODING=windows-1250",
+                  layer = "a05_vodni_nadrze") # i zde je možné velikost písmen ignorovat
+
 # jaký je rozdíl v načteném objektu oproti tomu ze skriptu 01?
 
 # výsledek nemá třídu tibble, která je mnohem přehlednější
@@ -29,3 +33,9 @@ nadrze <- nadrze |>
   st_sf()
 
 # podmínkou je přítomnost sloupce s geometrií
+
+# vybírání vrstev pomocí argumentu "layer" mimochodem pomáhá i při práci s druhým archivovaným objektem, kterým je pouhá atributová tabulka
+# především jde opět o řešení problému s kódováním znaků
+doplnek <- read_sf("/vsizip/geodata/dib_a05_vodni_nadrze.zip",
+                   options = "ENCODING=windows-1250",
+                   layer = "a05_doplnujici_charakteristiky")
