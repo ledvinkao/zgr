ce46f3312c6617b16b1f34a858e6e3fe3b946d92
Ondrej Ledvinka <stathydro.cz@gmail.com>
2025-09-22 15:47:27 +0200

    script 40 based on daemons from now on; some typos corrected as well

--- 40scripts/40_zisk_polygonu_povodi_nad_vodomernymi_stanicemi.R
+++ 40scripts/40_zisk_polygonu_povodi_nad_vodomernymi_stanicemi.R
@@ -1,5 +1,5 @@
 
-# Získ polygonů povodí nad vodoměrnými stanicemi v Česku ------------------
+# Zisk polygonů povodí nad vodoměrnými stanicemi v Česku ------------------
 
 # v hydrologii často potřebujeme pracovat s povodími nad vodoměrnými stanicemi
 # důvodem může být porovnání průtokové časové řady s řadami získanými z klimatologických měření (např. ve formě gridu)
@@ -10,8 +10,7 @@
 # načteme potřebné balíčky
 xfun::pkg_attach2("tidyverse",
                   "sf",
-                  "arcgislayers", 
-                  "multidplyr") # pro moderní paralelizaci s využitím tidyverse sloves a tabulek třídy tibble
+                  "arcgislayers")
 
 # získejme všechny nejmenší polygony povodí na podkladě informací na https://open-data-chmi.hub.arcgis.com/datasets/chmi::rozvodnice-povod%C3%AD-4-%C5%99%C3%A1du-roz%C5%A1%C3%AD%C5%99en%C3%A9/about
 # na konci si ještě upravujeme textové řetězce, aby správně obsahovaly znaky NA pro chybějící hodnoty (může se hodit i pro jinou práci)
@@ -22,7 +21,7 @@ catch <- arc_read("https://services1.arcgis.com/ZszVN9lBVA5x4VmX/arcgis/rest/ser
                 \(x) if_else(x == "", NA, x)))
 
 # zachovejme si tabulku jen s řádky, které obsahují řádky s ID vodoměrných stanic
-# využíváme vedlejšího efektu (nezvyklá ID, tj. některé dbcn, jsou konverovány na hodnoty NA uvnitř funkce filter())
+# využíváme vedlejšího efektu (nezvyklá ID, tj. některé dbcn, jsou konvertovány na hodnoty NA uvnitř funkce filter())
 # vybereme jen ty nejdůležitější sloupce před napojením části s geometriemi
 stations <- catch |> 
   st_drop_geometry() |> 
@@ -31,40 +30,41 @@ stations <- catch |>
          chp_14_s,
          chp_14_s_u)
 
-# připravíme se na paralelizovaný výpočet
+# připravíme se na paralelizovaný výpočet prostřednictvím funkce in_parallel() - viz dokumentaci k funkcím balíčku purrr
 catch2 <- catch |> 
   left_join(stations,
             join_by(between(chp_14_s, # zde využíváme nové funkce join_by(), která není závislá jen na rovnosti klíčů
                             chp_14_s_u,
                             chp_14_s))) |> 
   filter(!is.na(dbcn.y)) |> 
-  group_by(dbcn.y) # grupování je důležité pro rozčlenění
+  nest(rest = -dbcn.y)
 
-# další část je inspirována vinětou na https://cran.rstudio.com/web/packages/multidplyr/vignettes/multidplyr.html
-cluster <- new_cluster(parallelly::availableCores() - 1) # jak je doporučeno ve vinětě; výkon závisí na konkrétním stroji
+# před spuštěním musí být zavoláni tzv. démoni, jejich počet je závislý na počtu logických jader toho kterého stroje
+# k tomnu potřebujeme následující funkce balíčku mirai
+mirai::daemons(0) # dáváme jen pro případ nutnosti přepnutí na sekvenční zpracování
 
-# rozčleníme si naší sf kolekci
-catch2 <- catch2 |> 
-  partition(cluster)
+mirai::daemons(parallelly::availableCores() - 1) # aby byl skript obecný, zjišťujeme nejprve počet dostupných jader a zmenšujeme jej o jedničku, abychom klidně mohli dělat ještě něco jiného
 
-# ukážeme otrokům, že využíváme funkce balíčku sf
-cluster_library(cluster,
-                "sf")
-
-# spustíme proces sjednocování polygonů po skupinách definovaných stanicemi
+# spustíme proces sjednocování polygonů po řádcích reprezentujících jednotlivé stanice
 # měříme též strávený čas
 # a nakonec si necháme zahrát fanfáru, která nás zvukově upozorní, že je vše hotové:-)
-tictoc::tic(); catch2 <- catch2 |> 
-  summarize(geoms = st_union(geometry)) |> # přesně tohle musí být spuštěno paralelně
-  collect() |> 
-  st_sf() |> 
-  rename(id = dbcn.y); tictoc::toc(); beepr::beep(3)
+# podle dokumentace je třeba se ještě ujistit, zda budeme moci proces paralelizovat
+mirai::require_daemons() # odteď by funkce map(), walk() apod. měly umožňovat paralelní spouštění funkcí uvnitř in_parallel()
 
-# teď již můžeme klastr odstranit
-rm(cluster)
+tictoc::tic(); catch3 <- catch2 |> 
+  mutate(rest = map(rest,
+                    in_parallel(\(x) {
+                      xfun::pkg_attach2("tidyverse",
+                                        "sf")
+                      st_union(x)
+                    }))) |> # neznámé objekty v dalších, než je hlavní proces, představujeme za definicí funkce
+  unnest(rest) |> 
+  st_sf() |> # protože výsledek funkce unnest() je tibble
+  st_set_geometry("geoms") |> # přejmenujeme sloupec s geometrií
+  rename(dbcn = dbcn.y); tictoc::toc(); beepr::beep(3)
 
 # vypočítáme si plochy nově vzniklých polygonů
-catch2 <- catch2 |> 
+catch3 <- catch3 |> 
   mutate(a = st_area(st_transform(geoms,
                                   3035)) |> 
            units::set_units("km2") |> 
@@ -72,11 +72,11 @@ catch2 <- catch2 |>
   arrange(desc(a)) # pro potřeby kreslení, je dobrým zvykem mít největší povodí vespod (tedy v atributové tabulce v řádcích na začátku)
 
 # zbavíme se povodí s nulovou plochou
-catch2 <- catch2 |> 
+catch3 <- catch3 |> 
   filter(a > units::set_units(0,
                               "km2"))
 
 # výsledný objekt uložíme do RDS, abychom s ním mohli v R pracovat dále
-write_rds(catch2,
+write_rds(catch3,
           "results/povodi_nad_738_stanicemi.rds",
           compress = "gz")
